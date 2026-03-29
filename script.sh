25284579b41b8d6a28b47ab612d89242886478c3
Pasindu Manahara <manahara2003@gmail.com>
2026-03-29 11:36:36 +0530

    Enhance script to manage Python environment and Ollama

--- script.sh
+++ script.sh
@@ -1,9 +1,41 @@
 #!/bin/bash
 
-# activate the virtual environment
-source env/bin/activate
+read -p "create new environment? -y -n" answer
 
+if [ $answer = "y" ]; then    
+    # create python env
+    python3 -m venv env
+    # activate the virtual environment
+    source env/bin/activate
+    # python packages
+    pip install "fastapi[standard]" ollama
+    echo "Done..."
+else
+    read -p "path? [/env/]" path
+    if [ -z path ]; then
+        exit
+    source $path/actvate
+fi
+
+# install ollama
+read -p "already have ollama? -y -n" answer1
+
+if [ $answer1 = "n" ]; then
+    echo "installing ollama.."
+    sudo apt install ollama
+    echo "pulling llama3.2:3b model.."
+    ollama pull llama3.2:3b
+    addr=$(ollama serve | grep 127)
+    site=$(curl $addr | grep "ollama is running")
+    if [ $site != "ollama is running" ]; then 
+        echo "error starting ollama"
+        echo "exiting.."
+        exit
+    fi
+fi
+        
 # start a python server on port 5050
+echo "Starting HTTP server"
 python -m http.server 5050
 
 # run backend server
